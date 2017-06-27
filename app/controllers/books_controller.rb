fcbcc231d2e7fb0d6733308b785ec9cc22d49b73
jonathankim407 <jonathankim407@gmail.com>
2017-06-27 12:15:47 -0400

    step 3: add a plain text response
    books_controller.rb
    format.text
    
    index.text.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,11 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    respond_to :html
+
+    respond_to do |format|
+      format.html
+      format.text
+    end
   end
 
 end
